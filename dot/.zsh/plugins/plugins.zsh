3a8d3fdf61ab0e8dab1e7716c20f2758f22e2773
wxrdcn <wxrdcn@gmail.com>
2026-01-19 12:23:44 -0600

    x

--- dot/.zsh/plugins/plugins.zsh
+++ dot/.zsh/plugins/plugins.zsh
@@ -3,3 +3,8 @@ if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
     . /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
 fi
+
+#if [ -f /usr/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh ]; then
+#    . /usr/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+#fi
+
